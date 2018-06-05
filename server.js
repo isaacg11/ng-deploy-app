413e09c5c76a8375a25befef36de81290f5009c0
Isaac Grey <isaacgrey@Isaacs-MacBook-Pro.local>
2018-06-05 16:23:18 -0700

    3rd test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const path = require('path');
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -7,5 +8,9 @@ app.use(bodyParser.json())
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + "/dist/"));
+});
 
 app.listen(process.env.PORT || 8080);
